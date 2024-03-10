cf963e432d45a8bed84a10571b1f9aee97ca97c5
Eli Brunette <brunetteeli@gmail.com>
2024-03-09 21:12:33 -0800

    Adding a reference function app

---
+++ api/reference.py
@@ -0,0 +1,25 @@
+import azure.functions as func
+import logging
+import json
+from helper.storageAccount import login_storage_account
+
+def get_athlete(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    login_storage_account()    
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(json.dumps({"isAuthenticated":True}), status_code=200)
+    else:
+        return func.HttpResponse(
+             json.dumps({"isAuthenticated":True}),
+             status_code=200
+        )
