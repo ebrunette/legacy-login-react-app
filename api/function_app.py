443ca7cbd811ad89b6c9ee2a590a36d883436afe
Eli Brunette <brunetteeli@gmail.com>
2024-03-09 21:52:48 -0800

    Connecting to storage account and loading into df

--- api/function_app.py
+++ api/function_app.py
@@ -9,23 +9,23 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 app = func.FunctionApp()
 
 @app.route(route="login", auth_level=func.AuthLevel.FUNCTION)
-def get_athlete(req: func.HttpRequest) -> func.HttpResponse:
+def login(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     login_storage_account()    
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(json.dumps({"isAuthenticated":True}), status_code=200)
-    else:
-        return func.HttpResponse(
-             json.dumps({"isAuthenticated":True}),
-             status_code=200
-        )
+    # if name:
+    #     return func.HttpResponse(json.dumps({"isAuthenticated":True}), status_code=200)
+    # else:
+    return func.HttpResponse(
+            json.dumps({"isAuthenticated":True}),
+            status_code=200
+    )
--- api/helper/storageAccount.py
+++ api/helper/storageAccount.py
@@ -1,6 +1,26 @@
 import os
 import logging
+import pandas as pd
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 
 def login_storage_account():
-    blob_connection_string = os.environ["AZURE_BLOB_STORAGE_CONNECTION_STRING"]
-    logging.info(blob_connection_string)
+    
+    credential = DefaultAzureCredential()
+    
+    # Retrieve the storage blob service URL, which is of the form
+    # https://<your-storage-account-name>.blob.core.windows.net/
+    storage_account_name = os.environ["STORAGE_ACCOUNT_NAME"]
+    account_key = os.environ["STORAGE_ACCOUNT_KEY"]
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+    
+    container_name = "loginappstorage"
+    blob_name = "web_app.csv"
+    
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+
+    # load to RAM, eg. jupyter notebook
+    athlete_df = pd.read_csv(blob_client.download_blob())
+    logging.info(athlete_df.head())
