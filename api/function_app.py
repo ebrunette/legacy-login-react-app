677ff8a5350ee64152d2f469cbb72fe4229e1ec3
Eli Brunette <brunetteeli@gmail.com>
2024-03-10 01:46:10 -0800

    Finished validation method

--- api/function_app.py
+++ api/function_app.py
@@ -7,23 +7,26 @@ app = func.FunctionApp()
 
 @app.route(route="login", auth_level=func.AuthLevel.FUNCTION)
 def login(req: func.HttpRequest) -> func.HttpResponse:
-        logging.info('Python HTTP trigger function processed a request.')
-
-
-        if not name:
-                try:
-                        req_body = req.get_json()
-                except ValueError:
-                        pass
-                else:
-                        name = req_body.get('name')
-
+        isValidated = False
+        athlete_id = None
+        
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            athlete_id = req_body.get('athlete_id')
+        logging.info(athlete_id)
+        
         athletes_df = return_athletes()
+        
+        if len(athletes_df[athletes_df['UserId'] == athlete_id]) == 1:
+                isValidated = True
 
-        if name:
+        if isValidated:
                 return func.HttpResponse(json.dumps({"isAuthenticated":True}), status_code=200)
         else:
                 return func.HttpResponse(
-                json.dumps({"isAuthenticated":True}),
+                json.dumps({"isAuthenticated":False}),
                 status_code=200
                 )
