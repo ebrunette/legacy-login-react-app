fcebc752fc2e3fb5d240da940499be71c1aa65d5
Eli Brunette <brunetteeli@gmail.com>
2024-03-24 19:58:27 -0700

    Working out newathlete endpoint

--- api/function_app.py
+++ api/function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 import json
 import logging
+import pandas as pd
 
 from helper.storageAccount import return_athletes
 
@@ -56,7 +57,6 @@ def forgotid(req: func.HttpRequest) -> func.HttpResponse:
         email = req_body.get('email')
     
     athletes_df = return_athletes()
-    logging.info(athletes_df.columns)
     athletes = athletes_df[(athletes_df['FirstName'] == athlete_first_name) & 
                            (athletes_df['LastName'] == athlete_last_name) & 
                            (athletes_df['AccountFirstName'] == guardian_first_name) & 
@@ -75,3 +75,56 @@ def forgotid(req: func.HttpRequest) -> func.HttpResponse:
              "Please insert a valid first name",
              status_code=200
         )
+
+@app.route(route="newathlete", auth_level=func.AuthLevel.FUNCTION)
+def newathlete(req: func.HttpRequest) -> func.HttpResponse:
+    athlete_id = None
+    
+    try:
+            req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        athlete_first_name = req_body.get('first_name')
+        athlete_last_name = req_body.get('last_name')
+        guardian_first_name = req_body.get('guardian_first_name')
+        guardian_last_name = req_body.get('guardian_last_name')
+        if guardian_first_name == "" or guardian_first_name is None: 
+            guardian_first_name = athlete_first_name
+        if guardian_last_name == "" or guardian_last_name is None: 
+            guardian_last_name = athlete_last_name
+        email = req_body.get('email')
+
+    athletes_df = return_athletes()
+    logging.info(len(athletes_df))
+    
+    athletes = athletes_df[(athletes_df['FirstName'] == athlete_first_name) & 
+                           (athletes_df['LastName'] == athlete_last_name) & 
+                           (athletes_df['AccountFirstName'] == guardian_first_name) & 
+                           (athletes_df['AccountLastName'] == guardian_last_name)
+                           ]
+    if len(athletes) > 0: 
+        logging.info("Athlete already exists.")
+        athlete_id = list(athletes['UserId'])[0]
+    else:
+        # write to new athlete file 
+        # 
+        max_athlete_id = athletes_df['UserId'].max()
+        new_athlete_id = max_athlete_id + 1
+        
+        new_athlete_df = pd.DataFrame([["",
+                                            "",
+                                            "",
+                                            guardian_first_name,
+                                            guardian_last_name,
+                                            athlete_first_name,
+                                            athlete_last_name,
+                                            str(new_athlete_id)
+                                        ]], columns=athletes_df.columns)
+        final_df = pd.concat([new_athlete_df, 
+                              athletes_df], ignore_index= True)
+        final_df.to_csv("./data/web_app.csv", index=False)
+        
+    return func.HttpResponse(json.dumps({"athlete_id": str(new_athlete_id),
+                                                "athlete_name": athlete_first_name}), 
+                                         status_code=200)
